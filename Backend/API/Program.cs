0e7a217d66248fd1c03a96192f1def147f1375a7
Divya Singh <singhdivya.2895@gmail.com>
2023-09-07 04:47:56 +0000

    created a domain entity, Adding an EntityFramework Db Context, Created an EntityFramework code first Migration, created a database ,seeding data into database , Added an Api Controller ched using postman

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,3 +1,6 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,8 +23,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
